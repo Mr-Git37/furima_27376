dfbab1240915114da9a2fdfab1a949a33966384b
Mr-Git37 <gits0624@gmail.com>
2020-07-29 07:15:03 +0900

    正規表現を変数に定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,13 +7,13 @@ class User < ApplicationRecord
   with_options presence: true do
     number = /\A[ぁ-んァ-ン一-龥]/
     plus = /\A[ァ-ヶー－]+\z/
-    α = /\A[a-zA-Z0-9]+\z/
+    place = /\A[a-zA-Z0-9]+\z/
     
     validates :nickname
     validates :firstname, format: { with: number, message: "First name Full-width characters"}
     validates :lastname, format: { with: number, message: "Last name Full-width characters"}
     validates :firstfurigana, format: { with: plus, message: "Firstfurigana Full-width characters"}
-    validates :lastfurigana, format: { with: plus, message: "Lastfurigana Full-width characters"}
+    validates :lastfurigana, format: { with: place, message: "Lastfurigana Full-width characters"}
     validates :birthday
   end
   
