d162fa191eec8986f774ea44de2b1b5c8327e1a7
Mr-Git37 <gits0624@gmail.com>
2020-07-28 21:11:57 +0900

    コメントアウト削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,10 +17,4 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :firstname, :lastname, :firstfurigana, :lastfurigana, :birthday])
   end
-
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
-  end
 end
