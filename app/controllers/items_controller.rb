fb6ed301ecc0b1d7975c92f90c55af516dbc606c
Mr-Git37 <gits0624@gmail.com>
2020-08-01 21:01:01 +0900

    DB保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,10 +4,21 @@ class ItemsController < ApplicationController
   end
 
   def new
-    # @item = Item.new
+    @item = Item.new
   end
 
   def create
-    @item = Item.new(sign_in_params)
+    @item = Item.new(item_params)
+    @item.save
+    if @item.save
+      redirect_to root_path, notice: '出品完了しました'
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:title, :text, :price, :genre_id, :condition_id,:deliveryfee_id, :prefecture_id, :scheduleddelivery_id)
   end
 end
