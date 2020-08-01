bfebd8a0640c4fd193720793952bf7d86feab8d7
Mr-Git37 <gits0624@gmail.com>
2020-08-01 10:55:29 +0900

    /items/new　ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  get 'articles/index'
+  get 'articles/new'
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: "users/sessions",
   }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root  'items#index'
-  resources :tweets, only: [:index, :new, :create]
+  resources :items,only: [:index, :new, :create]
+  # get 'posts', to: 'posts#index'
+    resources :articles,only: :show
 end
 
