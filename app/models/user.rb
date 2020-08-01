3d2bab369e5ea6df8870e72a375b2baded29ce32
Mr-Git37 <gits0624@gmail.com>
2020-08-01 10:59:09 +0900

    validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,13 +10,13 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname
-    validates :firstname, format: { with: number, message: "First name Full-width characters"}
-    validates :lastname, format: { with: number, message: "Last name Full-width characters"}
-    validates :firstfurigana, format: { with: plus, message: "Firstfurigana Full-width characters"}
-    validates :lastfurigana, format: { with: place, message: "Lastfurigana Full-width characters"}
+    validates :firstname, format: { with: number, message: "Full-width characters"}
+    validates :lastname, format: { with: number, message: "Full-width characters"}
+    validates :firstfurigana, format: { with: plus, message: "Full-width characters"}
+    validates :lastfurigana, format: { with: plus, message: "Full-width characters"}
     validates :birthday
   end
   
   validates :password, format: { with: place,
-    message: "Password Include both letters and numbers" }
+    message: "Include both letters and numbers" }
 end
