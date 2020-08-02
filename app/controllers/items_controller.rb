1b41d6bd3f7127a384d93258f16591932efb7088
Mr-Git37 <gits0624@gmail.com>
2020-08-02 17:05:33 +0900

    不要

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,7 +11,7 @@ class ItemsController < ApplicationController
     @item = Item.new(item_params)
     @item.save
     if @item.save
-      redirect_to root_path, notice: '出品完了しました'
+      redirect_to root_path
     else
       render :new
     end
