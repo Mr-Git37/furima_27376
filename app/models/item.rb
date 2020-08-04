f87bd2fa47cc9a0990ea9a51111c4e13c8931723
Mr-Git37 <gits0624@gmail.com>
2020-08-04 14:55:06 +0900

    ActiveStorage

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :genre
+  has_one_attached :image
 
   #空の投稿を保存できないようにする
   # validates :title, :text, :genre, :image, presence: true
