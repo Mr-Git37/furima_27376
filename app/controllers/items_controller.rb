82ce218231072f4273c60775e01924945dbccc7a
Mr-Git37 <gits0624@gmail.com>
2020-08-04 14:55:49 +0900

    商品名,価格の表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,9 +5,7 @@ class ItemsController < ApplicationController
   # end
 
   def index
-    # @item = Item.find(current_item.id)
-    # @item = Item.find(params[:id])
-    @items = Item.includes(:images).order('created_at DESC').limit(5)
+    @items = Item.order('created_at DESC').limit(5)
     # binding.pry
   end
 
@@ -17,8 +15,7 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    # @item.save
-    if @item.save!
+    if @item.save
       redirect_to root_path
     else
       render :new
@@ -29,7 +26,7 @@ class ItemsController < ApplicationController
 
   private
   def item_params
-    params.require(:item).permit(:title, :text, :price, :genre_id, :condition_id,:deliveryfee_id, :prefecture_id, :scheduleddelivery_id)
+    params.require(:item).permit(:title, :text, :image, :price, :genre_id, :condition_id,:deliveryfee_id, :prefecture_id, :scheduleddelivery_id)
   end
 
 
