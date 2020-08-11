745e50dd6b9cdc6180aaa87bc1a7ea11276e8355
Mr-Git37 <gits0624@gmail.com>
2020-08-11 12:25:41 +0900

    不要

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,8 +3,5 @@ class Order < ApplicationRecord
   belongs_to_active_hash :prefecture
   has_many :items
   has_one_attached :image
-
-  # with_options precence: true do
-  #     validates :price
-  # end
+  
 end
